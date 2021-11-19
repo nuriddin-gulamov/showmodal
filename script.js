efd381cc8ca5ffccee4f09b51eb6ae1701156d86
Nuriddin <90389697+nuriddin-gulamov@users.noreply.github.com>
2021-11-19 19:55:51 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// Using Strict Mode
+"use strict";
+
+// Objects
+const modal = document.querySelector(".modal");
+const showModal = document.querySelectorAll(".show-modal");
+const closeModal = document.querySelector(".close-modal");
+const overlay = document.querySelector(".overlay");
+
+// Project Logic
+showModal.forEach(function (button) {
+    button.addEventListener("click", function () {
+        modal.classList.remove("hidden");
+        overlay.classList.remove("hidden");
+
+        closeModal.addEventListener("click", function () {
+            modal.classList.add("hidden");
+            overlay.classList.add("hidden"); 
+        });
+
+        overlay.addEventListener("click", function () {
+            modal.classList.add("hidden");
+            overlay.classList.add("hidden"); 
+        });
+    });
+});
